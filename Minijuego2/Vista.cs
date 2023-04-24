9e8e275f543af9195fc9cdade00dd0474fe8142b
Omar Moyano <omarmoyano7w@gmail.com>
2023-04-24 15:19:22 -0300

    Update Vista.cs

--- Minijuego2/Vista.cs
+++ Minijuego2/Vista.cs
@@ -7,7 +7,7 @@ namespace JuegoRetro
 {
     internal class Vista
     {
-        public void MostrarMenu()
+        public int MostrarMenu()
         {
             GameLoop gameLoop = new GameLoop();
             int opcion;
@@ -17,15 +17,16 @@ namespace JuegoRetro
                 Console.Clear();
                 Ventana.DibujarMarco();
                 Escritor.Escribir("¡Hola, 'P'layer espero que estes listo para perder!", posX, posY, true);
-                Escritor.Escribir("En este minijuego tendrás que superar 3 laberintos, recogiendo todos los puntos", posX, posY + 1, true);
-                Escritor.Escribir("de cada nivel para que así se desbloquee la puerta(K) y puedas avanzar.", posX, posY + 2, true);
+                Escritor.Escribir("En este minijuego tendrás que superar 1 laberintos, recogiendo todos los puntos", posX, posY + 1, true);
+                Escritor.Escribir("de cada nivel para que así se desbloquee la puerta(¦¦) y puedas avanzar.", posX, posY + 2, true);
                 Escritor.Escribir("Pero no será tan sencillo, ya que un 'E'nemigo te estará persiguiendo >:)", posX, posY + 3, true);
-                Escritor.Escribir("[1] Jugar - [0] Salir", posX, posY + 4, true);
+                Escritor.Escribir("[1] Jugar", posX, posY + 4, true);
                 Console.SetCursorPosition(posX, posY + 5);
                 Console.CursorVisible = true;
-            } while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 0 || opcion > 1);
+            } while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 1);
 
-            if (opcion == 1) gameLoop.IniciarGameLoop();
+            if (opcion == 1) return gameLoop.IniciarGameLoop();
+            return 0;
         }
     }
 }
